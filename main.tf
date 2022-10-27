6f65461802b675e16857c9b907ce5a20d4d5992c
wedmund <57000220+wedmund@users.noreply.github.com>
2022-10-28 10:10:35 +1100

    fixed the location

--- main.tf
+++ main.tf
@@ -45,9 +45,10 @@ resource "aws_instance" "blog" {
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.13.1"
+  
   name = "blog"
-
   vpc_id              = module.vpc.public_subnets[0]
+  
   ingress_rules       = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
 
