70ce19adf6235f2be8d447017dfc1c09518d340c
wedmund <57000220+wedmund@users.noreply.github.com>
2022-10-28 10:30:07 +1100

    test

--- main.tf
+++ main.tf
@@ -46,12 +46,12 @@ module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.13.1"
   
-  name = "blog"
+  name                = "blog"
   vpc_id              = module.vpc.public_subnets[0]
 
   ingress_rules       = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
 
-  egress_rules = ["all-all"]
-  egress_cidr_blocks = ["0.0.0.0/0"]
+  egress_rules        = ["all-all"]
+  egress_cidr_blocks  = ["0.0.0.0/0"]
 }
