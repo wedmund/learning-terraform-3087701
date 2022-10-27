882e6682cca146b88d0bf7547c2636b6432deccc
wedmund <57000220+wedmund@users.noreply.github.com>
2022-10-28 10:22:54 +1100

    commit

--- main.tf
+++ main.tf
@@ -48,7 +48,7 @@ module "blog_sg" {
   
   name = "blog"
   vpc_id              = module.vpc.public_subnets[0]
-  
+
   ingress_rules       = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
 
