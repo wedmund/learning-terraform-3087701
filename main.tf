230fba31253d11b4f5e6b3ef6bbd4de6f3cc5507
wedmund <57000220+wedmund@users.noreply.github.com>
2022-10-28 09:56:49 +1100

    remove old security groups

--- main.tf
+++ main.tf
@@ -46,7 +46,7 @@ resource "aws_instance" "blog" {
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.13.1"
-  name = "blog_new"
+  name = "blog"
 
   vpc_id              = module.vpc.public_subnets[0]
   ingress_rules       = ["http-80-tcp","https-443-tcp"]
@@ -61,34 +61,4 @@ resource "aws_security_group" "blog" {
   description = "allow http and https in. Allow everything out."
 
   vpc_id = module.vpc.public_subnets[0]
-}
-
-resource "aws_security_group_rule" "blog_http_in" {
-  type        = "ingress"
-  from_port   = 80
-  to_port     = 80
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-
-  security_group_id = aws_security_group.blog.id
-}
-
-resource "aws_security_group_rule" "blog_https_in" {
-  type        = "ingress"
-  from_port   = 443
-  to_port     = 443
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-
-  security_group_id = aws_security_group.blog.id
-}
-
-resource "aws_security_group_rule" "blog_everything_out" {
-  type        = "egress"
-  from_port   = 0
-  to_port     = 0
-  protocol    = "-1"
-  cidr_blocks = ["0.0.0.0/0"]
-
-  security_group_id = aws_security_group.blog.id
 }
