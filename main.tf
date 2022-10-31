4802b5dff481e9b39a528cd430f7ba09100a08cc
wedmund <57000220+wedmund@users.noreply.github.com>
2022-10-31 17:09:38 +1100

    test run 2

--- main.tf
+++ main.tf
@@ -36,22 +36,11 @@ module "blog_sg" {
   version = "4.13.1"
   
   name                = "blog"
-  vpc_id              = module.dev.public_subnets[0]
+  vpc_id              = module.vpc.vpc_id
 
   ingress_rules       = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
 
   egress_rules        = ["all-all"]
   egress_cidr_blocks  = ["0.0.0.0/0"]
-}
-
-resource "aws_instance" "blog" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
-  
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
-  
-  tags = {
-    Name = "HelloWorld"
-  }
 }
