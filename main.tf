b7d911375d4ff49ab9c08620c6b2197f8160864e
wedmund <57000220+wedmund@users.noreply.github.com>
2022-10-31 15:34:40 +1100

    using vpc default

--- main.tf
+++ main.tf
@@ -47,7 +47,7 @@ module "blog_sg" {
   version = "4.13.1"
   
   name                = "blog"
-  vpc_id              = module.vpc.id
+  vpc_id              = data.aws_vpc.default.id
 
   ingress_rules       = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
