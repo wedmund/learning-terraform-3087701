e786ba6eb6aa1310777e9ba57dc072c55f2480ea
wedmund <57000220+wedmund@users.noreply.github.com>
2022-10-31 16:45:18 +1100

    test

--- main.tf
+++ main.tf
@@ -30,28 +30,3 @@ module "vpc" {
     Environment = "dev"
   }
 }
-
-resource "aws_instance" "blog" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
-  
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
-  
-  tags = {
-    Name = "HelloWorld"
-  }
-}
-
-module "blog_sg" {
-  source  = "terraform-aws-modules/security-group/aws"
-  version = "4.13.1"
-  
-  name                = "blog"
-  vpc_id              = [module.vpc.default_vpc_id]
-
-  ingress_rules       = ["http-80-tcp","https-443-tcp"]
-  ingress_cidr_blocks = ["0.0.0.0/0"]
-
-  egress_rules        = ["all-all"]
-  egress_cidr_blocks  = ["0.0.0.0/0"]
-}
