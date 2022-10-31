0ed2700ddf56b229ab046716a7daf81094f7e13b
wedmund <57000220+wedmund@users.noreply.github.com>
2022-10-31 16:55:32 +1100

    commit

--- main.tf
+++ main.tf
@@ -30,3 +30,30 @@ module "vpc" {
     Environment = "dev"
   }
 }
+
+module "blog_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "4.13.1"
+  
+  name                = "blog"
+  vpc_id              = "${module.vpc.vpc_id}"
+
+  ingress_rules       = ["http-80-tcp","https-443-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+
+  egress_rules        = ["all-all"]
+  egress_cidr_blocks  = ["0.0.0.0/0"]
+  depends_on          = [module.vpc]
+}
+
+resource "aws_instance" "blog" {
+  ami           = data.aws_ami.app_ami.id
+  instance_type = var.instance_type
+  
+  vpc_security_group_ids = [module.blog_sg.security_group_id]
+  
+  tags = {
+    Name = "HelloWorld"
+  }
+  depends_on    = [module.blog_sg]
+}
--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,11 @@
 output "vpc_id" {
-  value = "${module.vpc.vpc_id}"
+  value = "${module.vpc}"
+}
+
+output "instance_ami" {
+  value = aws_instance.blog.ami
+}
+
+output "instance_arn" {
+  value = aws_instance.blog.arn
 }
