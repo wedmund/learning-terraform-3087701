a81e6824e582c6feba16edf69d3b065bf4e22d22
wedmund <57000220+wedmund@users.noreply.github.com>
2022-10-31 18:28:47 +1100

    test run 13

--- main.tf
+++ main.tf
@@ -44,10 +44,10 @@ resource "aws_instance" "blog" {
 
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
-  version = "4.13.0"
+  version = "4.13.1"
   
   name                = "blog"
-  vpc_id              = module.vpc.public_subnets[0]
+  vpc_id              = module.vpc.vpc_id
 
   ingress_rules       = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
