0ef76f2469632dbc2b592b92672d7f396e9c31a6
wedmund <57000220+wedmund@users.noreply.github.com>
2022-10-31 17:08:08 +1100

    test run

--- main.tf
+++ main.tf
@@ -36,7 +36,7 @@ module "blog_sg" {
   version = "4.13.1"
   
   name                = "blog"
-  vpc_id              = "${module.vpc.vpc_id}"
+  vpc_id              = module.dev.public_subnets[0]
 
   ingress_rules       = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
