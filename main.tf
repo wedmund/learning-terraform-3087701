be86fa562d6e9ef7ced323fd3dc6098e6172db76
wedmund <57000220+wedmund@users.noreply.github.com>
2022-10-31 16:40:23 +1100

    commit

--- main.tf
+++ main.tf
@@ -47,7 +47,7 @@ module "blog_sg" {
   version = "4.13.1"
   
   name                = "blog"
-  vpc_id              = data.aws_vpc.default.id
+  vpc_id              = [module.vpc.default_vpc_id]
 
   ingress_rules       = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
