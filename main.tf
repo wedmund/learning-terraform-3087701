36b50a949710d7cdaf4b083b9ee0f3c151e07fd2
wedmund <57000220+wedmund@users.noreply.github.com>
2022-10-31 20:02:10 +1100

    test run 24

--- main.tf
+++ main.tf
@@ -36,7 +36,7 @@ resource "aws_instance" "blog" {
   instance_type = var.instance_type
   
   vpc_security_group_ids = [module.blog_sg.security_group_id]
-  subnet_id = module.vpc.public_subnets[0].subnet_id
+  subnet_id = "${element(module.vpc.public_subnets, 0)}"
   
   tags = {
     Name = "HelloWorld"
